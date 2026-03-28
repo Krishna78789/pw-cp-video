6e38c3330902fddadff952a6b2b7427419668286
Krishna <srikrishnavidyarthi@gmail.com>
2026-03-28 21:20:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
+api_id = "24320085"
+api_hash = "a9d8e5464a5ac65180bcb48fda55ec66"
 bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+auth_users = [7521593568,1727542582]
+sudo_users = [7521593568,1727542582]
