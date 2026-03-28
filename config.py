d314f96d7bc5a6737a5c27c4daf7fd231a9c809c
Krishna <srikrishnavidyarthi@gmail.com>
2026-03-28 21:25:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "24320085"
 api_hash = "a9d8e5464a5ac65180bcb48fda55ec66"
-bot_token = "bot token"
+bot_token = "7917640363:AAF29EU5DxJKsCmo5bXT7JPrRBbPttMZ2Vw"
 auth_users = [7521593568,1727542582]
 sudo_users = [7521593568,1727542582]
